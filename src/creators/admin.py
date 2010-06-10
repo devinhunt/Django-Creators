28a35786dcc30fc27b3d927fb5433f2a2865c3d6
Devin Hunt <devinhunt@gmail.com>
2010-06-10 15:27:56 +0100

    Models updated to match new chip arch

--- src/creators/admin.py
+++ src/creators/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from creators.models import *
 
-#admin.site.register(Alert);
+admin.site.register(Alert);
+admin.site.register(Creator);
+admin.site.register(Place);
+admin.site.register(Video);
+admin.site.register(EventChip);
+admin.site.register(CreatorChip);
--- src/creators/models.py
+++ src/creators/models.py
@@ -14,12 +14,26 @@ class Creator(models.Model):
     
     name = models.CharField(max_length = 140)
     kind = models.CharField(max_length = 140, choices = CREATOR_TYPES)
-    thumbnail = models.ImageField(upload_to = "image/%Y/%m/%d")
+    thumbnail = models.ImageField(upload_to = "image/upload/%Y/%m/%d")
+    
+    def __unicode__(self):
+        return self.name + " (" + self.kind + ")";
     
 class Place(models.Model):
     name = models.CharField(max_length = 140)
     
-class Event(models.Model):
+    def __unicode__(self):
+        return self.name
+    
+class Video(models.Model):
+    creator = models.ForeignKey(Creator)
+    video = models.FileField(upload_to = "video/%Y/%m/%d")
+    title = models.CharField(max_length = 140)
+    
+    def __unicode__(self):
+        return self.title + " for " + self.creator.name
+    
+class EventChip(models.Model):
     creator = models.ForeignKey(Creator)
     name = models.CharField(max_length = 140)
     description = models.TextField()
@@ -27,7 +41,13 @@ class Event(models.Model):
     start = models.DateTimeField(auto_now_add = True)
     end = models.DateTimeField(auto_now_add = True)
     
-class Video(models.Model):
+    def __unicode__(self):
+        return self.name + " with " + self.creator.name
+
+class CreatorChip(models.Model):
     creator = models.ForeignKey(Creator)
-    video = models.FileField(upload_to = "video/%Y/%m/%d")
-    title = models.CharField(max_length = 140)
+    video = models.ForeignKey(Video, blank = True)
+    realtedChips = models.ForeignKey('self', blank = True)
+    
+    def __unicode__(self):
+        return self.creator.name + "chip"
