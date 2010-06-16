e10589172c33bd12f168d89703c11505e6bd1bf0
Devin Hunt <devinhunt@gmail.com>
2010-06-16 11:37:57 +0100

    Little admin changes

--- src/creators/admin.py
+++ src/creators/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from creators.models import *
 
+class RoomInline(admin.TabularInline):
+    model = Room
+
+class FloorAdmin(admin.ModelAdmin):
+    inlines = [
+        RoomInline,
+    ]
+
 admin.site.register(Alert);
 admin.site.register(Creator);
-admin.site.register(Floor);
+admin.site.register(Floor, FloorAdmin);
 admin.site.register(Room);
 admin.site.register(Video);
 admin.site.register(EventChip);
