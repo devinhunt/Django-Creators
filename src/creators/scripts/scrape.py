dc8d54756129b2a53d0b6c2356674616e37fba67
Devin Hunt <devinhunt@gmail.com>
2010-06-19 03:20:44 +0100

    Fixed the scraper

--- src/creators/scripts/scrape.py
+++ src/creators/scripts/scrape.py
@@ -1,36 +1,45 @@
 import sys, urllib, json
 from creators.models import *
 
-vice_url = "http://www.thecreatorsproject.com/api/creators.json"
+creators_url = "http://www.thecreatorsproject.com/api/creators.json"
 
 def scrape():
-    print "Starting a scrape of %s" % vice_url
-    creators_ref = urllib.urlopen(vice_url)
+    scrapeCreators()
+    scrapeEvents()
+
+def scrapeCreators():
+    print "Starting a scrape of %s" % creators_url
+    creators_ref = urllib.urlopen(creators_url)
     creators_obj = json.load(creators_ref)
     
     for creator_obj in creators_obj:
-        creator = Creator(name = creator_obj["name"], location = creator_obj["location"], remote_thumb = creator_obj["small-chip"])
-        creator.save();
+        try:
+            creator = Creator.objects.get(name = creator_obj["name"])
+        except:
+            creator = Creator(name = creator_obj["name"], location = creator_obj["location"], remote_thumb = creator_obj["small-chip"])
+            creator.save();
         
-        for theme_str in creator_obj["themes"]:
-            try:
-                theme = CreatorTheme.objects.get(type = theme_str)
-            except:
-                theme = CreatorTheme(type = theme_str)
-                theme.save()
+            for theme_str in creator_obj["themes"]:
+                try:
+                    theme = CreatorTheme.objects.get(type = theme_str)
+                except:
+                    theme = CreatorTheme(type = theme_str)
+                    theme.save()
             
-            creator.themes.add(theme)
+                creator.themes.add(theme)
+            print "Added %s" % creator.__unicode__();
         
-        print "Added %s" % creator.__unicode__();
+def scrapeEvents():
+    pass
     
 def main(*args):
-    #try:
-    scrape()
-    #except:
-    #    print "There was an error"
-    #    pass
-    #else:
-    return 0
+    try:
+        scrape()
+    except:
+        print "There was an error scrapping."
+        pass
+    else:
+        return 0
 
 if __name__ == "__main__":
     sys.exit(main(*sys.argv))
