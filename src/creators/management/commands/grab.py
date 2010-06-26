b00408bfdd548ca7de43dcdaad73228c9262d3a6
root <root@domU-12-31-39-03-A8-D6.compute-1.internal>
2010-06-26 19:53:44 +0000

    Playing with the grabber

--- src/creators/management/commands/grab.py
+++ src/creators/management/commands/grab.py
@@ -1,6 +1,6 @@
 import sys, urllib, json
 from django.core.management.base import BaseCommand, CommandError
-from datetime import datetime
+from datetime import datetime, timedelta
 from creators.models import Status, Room
 
 class Command(BaseCommand):
@@ -10,17 +10,18 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         latest_status = Status.objects.all().order_by("-pk")[0]
         print latest_status.pk
+	stupid_time = latest_status.created - timedelta(hours = 5)
         time_string = latest_status.created.strftime('%Y-%m-%d %H:%M:%S')
-        status_url = "http://at.thecreatorsproject.com/party/interface.php?type=list_all_users&by_time=" + time_string
+        time_date = stupid_time.strftime('%Y-%m-%d') + '%20' + stupid_time.strftime('%H:%M:%S')
+        status_url = "http://at.thecreatorsproject.com/party/interface.php?type=list_all_users&by_time=" + time_date
         print status_url
         status_ref = urllib.urlopen(status_url)
         status_objs = json.load(status_ref)
-        
-        
-        
+                
         if status_objs:
             for s_obj in status_objs:
+		print s_obj
                 if s_obj['status']:
-                    status = Status(status = s_obj['status'], created = datetime.today(), user = s_obj['username'], room = Room.objects.all()[0])
+                    status = Status(status = s_obj['status'], created = datetime.utcnow(), user = s_obj['username'], room = Room.objects.all()[0])
                     print status
                     status.save()
