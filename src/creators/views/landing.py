74c4addcd4a07f4a8946451c43c21a21566d1d23
Devin Hunt <devinhunt@gmail.com>
2010-07-15 23:48:19 +0100

    Basic objects being dumped into the news view

--- src/creators/views/landing.py
+++ src/creators/views/landing.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from creators.models import *
 
 
 def index(request):
     return render_to_response('index.html', {})
     
 def news(request):
-    return render_to_response('news.html', {})
+    minor_statuses = Status.objects.filter(state = 'minor').order_by('-created')[:40]
+    try:
+        major_status = Status.objects.filter(state = 'major').order_by('-created')[0]
+    except:
+        major_status = None
+    return render_to_response('news.html', {'statuses' : minor_statuses, 'major_status' : major_status})
 
 # Urls
 
