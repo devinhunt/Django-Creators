6de8efcba3b219edfa601684a105ba5bc9cdb3ce
Devin Hunt <devinhunt@gmail.com>
2010-07-16 01:14:21 +0100

    News with a little bit of style

--- src/creators/views/landing.py
+++ src/creators/views/landing.py
@@ -7,12 +7,8 @@ def index(request):
     return render_to_response('index.html', {})
     
 def news(request):
-    minor_statuses = Status.objects.filter(state = 'minor').order_by('-created')[:40]
-    try:
-        major_status = Status.objects.filter(state = 'major').order_by('-created')[0]
-    except:
-        major_status = None
-    return render_to_response('news.html', {'statuses' : minor_statuses, 'major_status' : major_status})
+    minor_statuses = Status.objects.exclude(state = 'dead').order_by('-created')[:40]
+    return render_to_response('news.html', {'statuses' : minor_statuses} )
 
 # Urls
 
